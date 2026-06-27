ab5a78fe96242a35007a2e5b63ec4df3b86a3a28
Ajay kowkuntla <ajaykowkuntla@gmail.com>
2026-06-27 22:44:04 +0530

    security: move firebase config credentials to EXPO_PUBLIC environment variables

--- mobile-wrapper/src/firebase/config.js
+++ mobile-wrapper/src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA1g7eO9dC7yn-gb1NzSeP9eS2c5SiLoAc',
-  authDomain: 'wewooo.firebaseapp.com',
-  databaseURL: 'https://wewooo-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'wewooo',
-  storageBucket: 'wewooo.firebasestorage.app',
-  messagingSenderId: '585420940759',
-  appId: '1:585420940759:web:6ad2a6d602fc33bc621792',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
